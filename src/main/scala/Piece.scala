d67a35ce2d64796788ca22b69b76bfa8331ab69e
jedikarix <karolchecinski@wp.pl>
2018-05-24 17:18:58 +0200

    Add pieces types

--- src/main/scala/Piece.scala
+++ src/main/scala/Piece.scala
@@ -1,10 +1,43 @@
 package chess.board
 
-abstract class Piece() {
-  val position: Point
-  val moved: Boolean
+sealed trait PieceType {
+  val name: String
+  val symbol: Char
+  val promotable: Boolean
+}
 
-  def moves(): List[Point]
-  def captures(): List[Point]
-  def toChar: Char
+case object Pawn extends PieceType {
+  val name = "Pawn"
+  val symbol = 'p'
+  val promotable = false
+}
+
+case object Knight extends PieceType {
+  val name = "Knight"
+  val symbol = 'N'
+  val promotable = true
+}
+
+case object Bishop extends PieceType {
+  val name = "Bishop"
+  val symbol = "B"
+  val promotable = true
+}
+
+case object Rook extends PieceType {
+  val name = "Rook"
+  val symbol = "R"
+  val promotable = true
+}
+
+case object Queen extends PieceType {
+  val name = "Queen"
+  val symbol = "Q"
+  val promotable = true
+}
+
+case object King extends PieceType {
+  val name = "King"
+  val symbol = "K"
+  val promotable = false
 }
