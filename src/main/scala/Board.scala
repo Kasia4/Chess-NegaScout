08fe130654769cd2fbd4bdf7e6993294c770bf95
jedikarix <karolchecinski@wp.pl>
2018-05-26 15:19:47 +0200

    Add ofColor and ofType method

--- src/main/scala/Board.scala
+++ src/main/scala/Board.scala
@@ -3,5 +3,8 @@ package chess.board
 case class Board (pieces: Map[Point, Piece] = Map()) {
   def add(pos: Point, piece: Piece) = Board(pieces + (pos -> piece))
   def remove(pos: Point) = Board(pieces - pos)
+  def getAt(pos: Point):Option[Piece] = pieces.get(pos)
+  def ofColor(color: Color): Map[Point, Piece] = pieces.filter(p => p._2.color == color)
+  def ofType(pieceType: PieceType): Map[Point, Piece] = pieces.filter(p => p._2.ptype == pieceType)
 }
 
--- src/test/scala/BoardTest.scala
+++ src/test/scala/BoardTest.scala
@@ -8,9 +8,42 @@ class BoardTest extends org.scalatest.FunSuite{
   }
 
   test("When piece is removed from board, pieces map doesn't contain it") {
+    val board = Board().add(Point(5,5), Piece(Pawn, Black))
+    val board2 = board.remove(Point(5,5))
+    assert(board2.pieces.get(Point(5,5)).isEmpty)
+  }
+
+  test("when piece is added to board, getAt returns that piece") {
+    val pos = Point(3,3)
+    val board = Board().add(pos, Piece(Pawn, Black))
+    assert(board.getAt(pos).get == Piece(Pawn, Black))
+  }
+
+  test("when field is empty, getAt returns None") {
+    val pos = Point(3,3)
     val board = Board()
-    val board2 = board.add(Point(5,5), Piece(Pawn, Black))
-    val board3 = board.remove(Point(5,5))
-    assert(board3.pieces.get(Point(5,5)).isEmpty)
+    assert(board.getAt(pos).isEmpty)
+  }
+
+  test("ofColor returns all pieces of given color") {
+    val board = Board()
+      .add(Point(0,1), Piece(Pawn, White))
+      .add(Point(0,0), Piece(Rook, White))
+      .add(Point(2,7), Piece(Pawn, Black))
+    val filtered = board.ofColor(White)
+    assert(filtered.size == 2)
+    assert(filtered.contains(Point(0,1)))
+    assert(filtered.contains(Point(0,0)))
+  }
+
+  test("ofType returns all pieces of given type") {
+    val board = Board()
+      .add(Point(0,1), Piece(Pawn, White))
+      .add(Point(0,0), Piece(Rook, White))
+      .add(Point(2,7), Piece(Pawn, Black))
+    val filtered = board.ofType(Pawn)
+    assert(filtered.size == 2)
+    assert(filtered.contains(Point(0,1)))
+    assert(filtered.contains(Point(2,7)))
   }
 }
