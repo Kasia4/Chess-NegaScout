a260f4d91f12d10cf58438d3a22fe4287d1db4d0
jedikarix <karolchecinski@wp.pl>
2018-05-29 17:50:54 +0200

    Add GameState displaying

--- src/main/scala/ui/UI.scala
+++ src/main/scala/ui/UI.scala
@@ -1,5 +1,10 @@
 package ui
 
-object UI {
+import chess.board.{GameState, Point}
 
+object UI {
+  def displayGame(game_state: GameState) :Unit = {
+    print(ui.Board.giveString(game_state.board, Set(Point(1,1), Point(1,3), Point(3, 3))))
+    println(game_state.result().message())
+  }
 }
