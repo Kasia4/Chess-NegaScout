4fc90e21713c534332cd67c0bd3f84a32df4cbe7
jedikarix <karolchecinski@wp.pl>
2018-05-31 14:44:46 +0200

    Add fields parsing

--- src/main/scala/chess/Notation.scala
+++ src/main/scala/chess/Notation.scala
@@ -1,9 +1,9 @@
 package chess
 
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
 object Notation {
-  val columnValues: Map[Char, Int] = Map(
+  private val columnValues: Map[Char, Int] = Map(
     'A' -> 0,
     'B' -> 1,
     'C' -> 2,
@@ -16,5 +16,22 @@ object Notation {
 
   def columnValue(symbol: Char): Try[Int] = Try(columnValues(symbol))
 
+  def rowValue(symbol: Char): Try[Int] = {
+    val sym_int = symbol.asDigit
+    if (sym_int >= 1 && sym_int <= 8)
+      Success(sym_int - 1)
+    else
+      Failure(new IllegalArgumentException)
+  }
+
+  def parseField(in: String): Try[Point] = {
+    if (in.length() != 2) Failure(new IllegalArgumentException)
+    else {
+      val x = columnValue(in.charAt(0))
+      val y = rowValue(in.charAt(1))
+      if (x.isFailure || y.isFailure) Failure(new IllegalArgumentException)
+      else Success(Point(x.get, y.get))
+    }
+  }
 
 }
--- src/test/scala/NotationTest.scala
+++ src/test/scala/NotationTest.scala
@@ -1,4 +1,5 @@
 import chess._
+import javax.management.Notification
 
 class NotationTest extends org.scalatest.FunSuite {
   test("Column number is counted properly") {
@@ -6,9 +7,32 @@ class NotationTest extends org.scalatest.FunSuite {
     assert(Notation.columnValue(symbol = 'H').get == 7)
   }
 
-  test("Improper column causes fail value") {
+  test("Improper column causes failure value") {
     assert(Notation.columnValue(symbol = 'X').isFailure)
   }
 
+  test("Row number is counted properly") {
+    assert(Notation.rowValue(symbol = '1').get == 0)
+    assert(Notation.rowValue(symbol = '8').get == 7)
+  }
+
+  test("Improper row causes failure value") {
+    assert(Notation.rowValue(symbol = 'G').isFailure)
+    assert(Notation.rowValue(symbol = '!').isFailure)
+    assert(Notation.rowValue(symbol = '9').isFailure)
+    assert(Notation.rowValue(symbol = '0').isFailure)
+  }
+
+  test("Field is parsing to proper Point") {
+    assert(Notation.parseField(in = "B4").get == Point(1,3))
+  }
+
+  test("Field with improper length causes failure value") {
+    assert(Notation.parseField(in = "B12").isFailure)
+  }
+
+  test("Field with improper values causes failure value") {
+    assert(Notation.parseField(in = "K1").isFailure)
+  }
 
 }
