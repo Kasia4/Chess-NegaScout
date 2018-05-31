51f8c49529c1e4610fac2e60c5ad490b442c6c18
jedikarix <karolchecinski@wp.pl>
2018-05-31 12:41:51 +0200

    Add Color to Player

--- src/main/scala/game/Player.scala
+++ src/main/scala/game/Player.scala
@@ -1,7 +1,8 @@
 package game
 
-import chess.GameState
+import chess.{Color, GameState, Move}
 
 trait Player {
-  def executeMove(gameState: GameState): GameState
+  def color: Color
+  def executeMove(gameState: GameState): Move
 }
