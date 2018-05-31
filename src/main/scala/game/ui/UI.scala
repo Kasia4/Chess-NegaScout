ea77f2e6821784d927cc01ec7b5d537d444227cf
jedikarix <karolchecinski@wp.pl>
2018-05-31 18:05:11 +0200

    Add active fields list to displayGame

--- src/main/scala/game/ui/UI.scala
+++ src/main/scala/game/ui/UI.scala
@@ -4,8 +4,10 @@ import chess.{GameState, Point}
 import game.Prompt
 
 object UI {
-  def displayGame(game_state: GameState, prompt: Option[Prompt] = None) :Unit = {
-    print(BoardUI.giveString(game_state.board, Set(Point(1,1), Point(1,3), Point(3, 3))))
+  def displayGame(game_state: GameState,
+                  active_fiedls: Set[Point] = Set.empty[Point],
+                  prompt: Option[Prompt] = None) :Unit = {
+    print(BoardUI.giveString(game_state.board, active_fiedls))
     println(game_state.result().message())
     if (prompt.isDefined) {
       println(prompt.get.content + ": ")
