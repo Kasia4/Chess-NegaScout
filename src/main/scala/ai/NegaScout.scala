882945d9635dc29775360ae8a948af66d7c00bb8
Kasia4 <katarzyna.rybacka4@gmail.com>
2018-06-01 00:58:40 +0200

    Added main function

--- src/main/scala/ai/NegaScout.scala
+++ src/main/scala/ai/NegaScout.scala
@@ -6,29 +6,42 @@ class NegaScout(heuristic: Heuristic, maxDepth: Int = 10) {
   def negaScoutIter(state: GameState, alpha: Double, beta: Double, depth: Int): (Double, Option[Move]) = {
     if(state.isTerminated() || depth >= maxDepth)
       (heuristic.countStateValue(state), None)
+
     val firstMove = state.possibleMoves().head
+
     val bestMove = state.possibleMoves().map(makeMoveStatePair(_,state))
       .foldLeft(alpha, beta, None: Option[Move]) { (currentAlgParams, curr) => {
         if(currentAlgParams._1 >= currentAlgParams._2) currentAlgParams
+
         else if(curr._1 == firstMove){
           val score = - negaScoutIter(curr._2, -currentAlgParams._2, -currentAlgParams._1, depth+1)._1
+
           if(currentAlgParams._1 < score) {
             (score, currentAlgParams._2, Some(curr._1))
           }
+
           else
             currentAlgParams
         }
+
         else {
           val newAlpha = math.max(currentAlgParams._1, - negaScoutIter(curr._2, -currentAlgParams._1 - 1, -currentAlgParams._1, depth+1)._1)
-            if(currentAlgParams._1 < newAlpha &&  newAlpha < currentAlgParams._2) {
+
+          if(currentAlgParams._1 < newAlpha &&  newAlpha < currentAlgParams._2) {
               val score = - negaScoutIter(curr._2, -currentAlgParams._2, -newAlpha, depth+1)._1
               (score, currentAlgParams._2, Some(curr._1))
             }
+
             else currentAlgParams
         }
       }
     }
     (bestMove._1, bestMove._3)
   }
+
+  def negaScout(heuristic: Heuristic, gameState: GameState): (Double, Option[Move]) =
+    negaScoutIter(gameState, Double.NegativeInfinity, Double.PositiveInfinity, maxDepth)
+
   private def makeMoveStatePair(move: Move, initialState: GameState): (Move, GameState) = (move, initialState.applyMove(move).get)
+
 }
