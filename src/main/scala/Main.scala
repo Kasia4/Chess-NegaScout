deb92890ab1d037ecd41b4df5b7506efa75715ca
jedikarix <karolchecinski@wp.pl>
2018-05-31 19:10:20 +0200

    Add prototype game loop

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,12 @@
-import chess.GameState
+import chess.{GameState, White}
+import game.HumanPlayer
 
 object Main {
   def main(args: Array[String]): Unit = {
+    val player = HumanPlayer(White)
     val game = GameState()
+
+    player.executeMove(game)
 //    print(game.ui.Board.print(game.board, Some(Point(1,1))))
   }
 }
