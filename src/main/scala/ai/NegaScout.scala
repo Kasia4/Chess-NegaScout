ed8c99556e11cea9d7aedb6e1eb82aaac871b069
Kasia4 <katarzyna.rybacka4@gmail.com>
2018-05-31 14:48:36 +0200

    WIP Negascout

--- src/main/scala/ai/NegaScout.scala
+++ src/main/scala/ai/NegaScout.scala
@@ -1,5 +1,29 @@
 package ai
 
-class NegaScout {
+import chess.{GameState, Move}
 
+class NegaScout(heuristic: Heuristic, maxDepth: Int = 10) {
+  def negaScoutIter(state: GameState, depth: Int, alpha: Int, beta: Int): (Move,Int)= {
+    if(state.isTerminated() || depth >= maxDepth) {
+      return heuristic(state)
+    }
+    val moves = state.possibleMoves()
+    var a = alpha
+    var b = beta
+    //var bestMove
+    for(m <- moves) {
+      val child = state.applyMove(m)
+      val score = -negaScoutIter(child.get, depth + 1, -b, -a)._2
+      if (score > a && score < b/*check if first*/ ) {
+        a = -negaScoutIter(child.get, depth + 1, -b, -score)._2
+      }
+      state.undo()
+      a = if (a > score) a else score
+      if (a >= beta) {
+        return (m, a)
+      }
+      b = a + 1 // bestMove
+    }
+    return a
+  }
 }
