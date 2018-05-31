5c8eadc0f2d3926fabfdd95a06601e94decbc333
Kasia4 <katarzyna.rybacka4@gmail.com>
2018-06-01 00:47:13 +0200

    Added basic implementation of NegaScout

--- src/main/scala/ai/NegaScout.scala
+++ src/main/scala/ai/NegaScout.scala
@@ -3,27 +3,32 @@ package ai
 import chess.{GameState, Move}
 
 class NegaScout(heuristic: Heuristic, maxDepth: Int = 10) {
-  def negaScoutIter(state: GameState, depth: Int, alpha: Int, beta: Int): (Move,Int)= {
-    if(state.isTerminated() || depth >= maxDepth) {
-      return heuristic(state)
-    }
-    val moves = state.possibleMoves()
-    var a = alpha
-    var b = beta
-    //var bestMove
-    for(m <- moves) {
-      val child = state.applyMove(m)
-      val score = -negaScoutIter(child.get, depth + 1, -b, -a)._2
-      if (score > a && score < b/*check if first*/ ) {
-        a = -negaScoutIter(child.get, depth + 1, -b, -score)._2
+  def negaScoutIter(state: GameState, alpha: Double, beta: Double, depth: Int): (Double, Option[Move]) = {
+    if(state.isTerminated() || depth >= maxDepth)
+      (heuristic.countStateValue(state), None)
+    val firstMove = state.possibleMoves().head
+    val bestMove = state.possibleMoves().map(makeMoveStatePair(_,state))
+      .foldLeft(alpha, beta, None: Option[Move]) { (currentAlgParams, curr) => {
+        if(currentAlgParams._1 >= currentAlgParams._2) currentAlgParams
+        else if(curr._1 == firstMove){
+          val score = - negaScoutIter(curr._2, -currentAlgParams._2, -currentAlgParams._1, depth+1)._1
+          if(currentAlgParams._1 < score) {
+            (score, currentAlgParams._2, Some(curr._1))
+          }
+          else
+            currentAlgParams
+        }
+        else {
+          val newAlpha = math.max(currentAlgParams._1, - negaScoutIter(curr._2, -currentAlgParams._1 - 1, -currentAlgParams._1, depth+1)._1)
+            if(currentAlgParams._1 < newAlpha &&  newAlpha < currentAlgParams._2) {
+              val score = -negaScoutIter(curr._2, -currentAlgParams._2, newAlpha, depth+1)._1
+              (score, currentAlgParams._2, Some(curr._1))
+            }
+            else currentAlgParams
+        }
       }
-      state.undo()
-      a = if (a > score) a else score
-      if (a >= beta) {
-        return (m, a)
-      }
-      b = a + 1 // bestMove
     }
-    return a
+    (bestMove._1, bestMove._3)
   }
+  private def makeMoveStatePair(move: Move, initialState: GameState): (Move, GameState) = (move, initialState.applyMove(move).get)
 }
